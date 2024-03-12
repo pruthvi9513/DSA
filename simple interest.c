19ba7e622e02f335d7466012abd98923e17406d5
pruthvi9513 <131328451+pruthvi9513@users.noreply.github.com>
2024-03-12 19:53:54 +0530

    Add files via upload

---
+++ simple interest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    float p;
+    float t;
+    float r;
+    float sp;
+    printf("enter the p t r\n");
+    scanf("%f%f%f",&p,&t,&r);
+    sp=p*t*r/100;
+    printf("SP=%f",sp);
+
+}
