da56c638825d6d26f5f5fc602bf6df852469c18c
pruthvi9513 <131328451+pruthvi9513@users.noreply.github.com>
2024-03-12 19:16:55 +0530

    Add files via upload

---
+++ addition and avarage of two numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    float a;
+    float b;
+    float sum;
+    float avg;
+    printf("enter two values\n");
+    scanf("%f%f",&a,&b);
+    sum=a+b;
+    avg=sum/2;
+    printf("Sum=%f",sum);
+    printf("Avg=%f",avg);
+           }
