a4a4dedc96fe0bb7565f42c4e5af0be6cd7ab7b6
pruthvi9513 <131328451+pruthvi9513@users.noreply.github.com>
2024-03-12 19:57:56 +0530

    Add files via upload

---
+++ number is postive or negativw.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("number is postive %d",n);
+    }
+    else
+        printf("number is negative%d",n);
+}
