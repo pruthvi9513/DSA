d81b79eddfcea224cb54cecc1d1aebd468ac61a7
pruthvi9513 <131328451+pruthvi9513@users.noreply.github.com>
2024-03-14 13:35:49 +0530

    Add files via upload

---
+++ print kle tech.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for(i = 0; i < 5; i++) {
+        printf("KLE TECH \n"); // Replace "Your Name" with your actual name
+    }
+
+    return 0;
+}
